c20f33fd4f045726127dd563d442ae3f5dacff60
Michese <Michese48@mail.ru>
2020-11-08 23:52:28 +0300

    13

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 'use strict';
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
 const PORT = process.env.PORT || 3085;
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}!`);
