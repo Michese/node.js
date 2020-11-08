3464b112cd240ccc2156f648c68b377de5c254d6
Michese <Michese48@mail.ru>
2020-11-08 13:21:28 +0300

    5

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-'use strict';
-const obj = require('./user.js');
+'use strict'
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-obj.sayHello();
-console.log(obj);
+const server = http.createServer((req, res) => {
+
+    fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
+        try {
+            if (err) {
+                throw err;
+            }
+            console.log(Buffer.from(data).toString());
+            res.write(Buffer.from(data).toString(), err2 => {
+                try {
+                    if (err2) {
+                        throw err2;
+                    }
+                } catch {
+                    console.error(err2.message);
+                }
+
+            });
+            res.end();
+        } catch {
+            console.error(err.message);
+        }
+    });
+
+
+})
+
+server.listen(3085, () => {
+    console.log('hello, world');
+})
