3129860b172cc62dce077bf1659c092d2bb2ddd6
Michese <Michese48@mail.ru>
2020-11-08 23:06:44 +0300

    12

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict';
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3085;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}!`);
+})
